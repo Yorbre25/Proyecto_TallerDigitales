a44dd58493b71edb97a24d955b8e582daade93f2
Ingrid Vargas Badilla <invabadilla@estudiantec.cr>
2022-11-07 17:02:59 -0600

    Update Fetch.sv

--- Fetch.sv
+++ Fetch.sv
@@ -4,10 +4,10 @@ module Fetch(input clk, rst, output [31:0]data);
 		logic [31:0] q;
 		
 		Counter pc (clk, rst,1'b1,address);
-		Rom memoria1 (address,clk, q1);
-		Rom memoria2 (address+1,clk, q2);
-		Rom memoria3 (address+2,clk, q3);
-		Rom memoria4 (address+3,clk, q4);
+		ROM_Instructions memoria1 (address,clk, q1);
+		ROM_Instructions memoria2 (address+1,clk, q2);
+		ROM_Instructions memoria3 (address+2,clk, q3);
+		ROM_Instructions memoria4 (address+3,clk, q4);
 		
 		always @ (*)
 		begin
